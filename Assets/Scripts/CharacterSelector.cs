0f72944e91ebf9128aa5c8b5345ffe260cd61f08
Tim Pham <43245834+phamt007@users.noreply.github.com>
2024-03-22 18:37:58 -0700

    Jordan's Work

---
+++ Assets/Scripts/CharacterSelector.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelector : MonoBehaviour
+{
+    
+    //TEMP
+    public List<string> characterName = new List<string>();
+    public int counter;
+    public int slot;
+
+    public Text characterNameText;
+    void Start()
+    {
+        PlayerManager.instance.Players[slot].Class = characterName[counter];
+    }
+
+    public void nextName()
+    {
+        counter += 1;
+        if(counter == characterName.Count)
+        {
+            counter = 0;
+        }
+        characterNameText.text  = characterName[counter];
+        PlayerManager.instance.Players[slot].Class = characterName[counter];
+    }
+
+    public void prevName()
+    {
+        counter -= 1;
+        if(counter < 0)
+        {
+            counter = characterName.Count - 1;
+        }
+        characterNameText.text  = characterName[counter];
+        PlayerManager.instance.Players[slot].Class = characterName[counter];
+    }
+
+}
--- Assets/Scripts/Managers/PlayerManager.cs
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -10,6 +10,8 @@ public class PlayerManager : MonoBehaviour
     public static PlayerManager instance;
     public List<Player> Players = new List<Player>();
 
+    public List<CharacterSelector> slots = new List<CharacterSelector>();
+
     void Awake()
     {
         if (instance == null)
@@ -26,10 +28,14 @@ public class PlayerManager : MonoBehaviour
     {
         if (Players.Count == 4)
             return;
-      
+        List<Vector3> positions = new List<Vector3> { new Vector3(-6, 1, 0), new Vector3(-2, 1, 0), new Vector3(2.2f, 1, 0), new Vector3(6.2f, 1, 0) };
         Player player = new Player(Random.Range(1, 100));
-        Players.Add(player);
-        GameObject temp = Instantiate(Resources.Load<GameObject>("Prefabs/Character"), transform);
+        GameObject temp = Instantiate(Resources.Load<GameObject>("Prefabs/Classes/Mage"), transform);
+        temp.gameObject.transform.position = positions[Players.Count];
+        temp.gameObject.transform.localScale = new Vector3(0.75f, 0.75f, 0);
+        temp.SetActive(true);
+        Players.Add(temp.GetComponent<Player>());
+
         temp.name = "Player" + Players.Count;
     }
 
@@ -37,6 +43,22 @@ public class PlayerManager : MonoBehaviour
     {
         Players.RemoveAt(slot);
         Destroy(transform.GetChild(slot));
+    }
+
+    public void StartGame()
+    {
+        for (int i = 0; i < Players.Count; ++i)
+        {
+            GameObject temp = transform.GetChild(i).gameObject;
+            GameObject temp2 = Resources.Load<GameObject>("Prefabs/Classes/" + temp.GetComponent<Player>().Class);
+
+            for (int j = 0; j < 7; j++)
+            {
+                Destroy(temp.transform.GetChild(j).gameObject);
+                temp2.transform.GetChild(j).gameObject.transform.SetParent(temp.transform);
+            }
+
+        }
     }
 
 }
