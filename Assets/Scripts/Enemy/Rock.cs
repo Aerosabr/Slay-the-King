783edfd9dd37ccb0b26576ba09c556c4c595b4b7
Daniel Tran <dhtran.hy@gmail.com>
2024-03-24 22:33:40 -0700

    adding damage to exploding rocks

--- Assets/Scripts/Enemy/Rock.cs
+++ Assets/Scripts/Enemy/Rock.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Rock : MonoBehaviour
@@ -8,19 +7,34 @@ public class Rock : MonoBehaviour
     public float currentHealth;
     public Rigidbody2D rb;
 
-    // Reference to the Gold prefab you want to drop
     public GameObject goldPrefab;
+
+    private float explosionTimer = 10f;
+    public float explosionRadius = 15f;
+    private bool isExploded = false;
 
     void Start()
     {
         currentHealth = maxHealth;
         rb = GetComponent<Rigidbody2D>();
+        StartCoroutine(StartExplosionCountdown());
+    }
+
+    private IEnumerator StartExplosionCountdown()
+    {
+        yield return new WaitForSeconds(explosionTimer);
+
+        if (!isExploded && currentHealth > 0)
+        {
+            Explode();
+        }
     }
 
     public void Damaged(float amount)
     {
+        if (isExploded) return;
+
         currentHealth -= Mathf.Abs(amount);
-        // Assuming DamagePopup.Create is a method you've defined elsewhere and works as intended
         DamagePopup.Create(rb.transform.position, (int)Mathf.Abs(amount), false);
 
         if (currentHealth <= 0)
@@ -31,16 +45,35 @@ public class Rock : MonoBehaviour
 
     private void Die()
     {
-        DropGold();
-        Destroy(gameObject); // This will remove the rock from the scene
+        if (!isExploded)
+        {
+            DropGold();
+        }
+        Destroy(gameObject);
     }
 
     private void DropGold()
     {
-        if (goldPrefab != null)
+        Instantiate(goldPrefab, transform.position, Quaternion.identity);
+    }
+
+    private void Explode()
+    {
+        isExploded = true;
+
+        Collider2D[] hitColliders = Physics2D.OverlapCircleAll(transform.position, explosionRadius);
+        foreach (var hitCollider in hitColliders)
         {
-            // Instantiate the Gold at the rock's position and no rotation
-            Instantiate(goldPrefab, transform.position, Quaternion.identity);
+            if (hitCollider.CompareTag("Player"))
+            {
+                PlayerHealth playerHealth = hitCollider.GetComponent<PlayerHealth>();
+                if (playerHealth != null)
+                {
+                    playerHealth.ChangeHealth(-5);
+                }
+            }
         }
+
+        Destroy(gameObject);
     }
 }
