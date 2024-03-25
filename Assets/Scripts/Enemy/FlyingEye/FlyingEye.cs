2d5ab722d0d140ab71de993c48f0e69dd1e0c103
Jordan <jhn100550@gmail.com>
2024-03-24 22:56:41 -0700

    Enemy spawner + enemy

---
+++ Assets/Scripts/Enemy/FlyingEye/FlyingEye.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingEye : MonoBehaviour
+{
+    public int maxHealth;
+    public int currentHealth;
+    public Animator anim;
+    public Rigidbody2D rb;
+    public float speed = 1.0f;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        if (currentHealth > 0)
+            transform.position = Vector2.MoveTowards(transform.position, GameObject.Find("Player1").transform.GetChild(0).transform.position, step);
+    }
+
+    public void Damaged(int amount)
+    {
+        currentHealth -= Mathf.Abs(amount);
+        if (currentHealth <= 0)
+        {
+            anim.SetTrigger("Death");
+            Destroy(rb);
+            Destroy(GetComponent<BoxCollider2D>());
+            Destroy(gameObject, 2f);
+        }
+        else
+            anim.SetTrigger("Damaged");
+            
+        DamagePopup.Create(rb.transform.position, Mathf.Abs(amount), false);
+    }
+}
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    private void Awake()
+    {
+        SpawnEnemy(Resources.Load<GameObject>("Prefabs/FlyingEye"));
+    }
+
+    public IEnumerator Spawning(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SpawnEnemy(Resources.Load<GameObject>("Prefabs/FlyingEye"));
+    }
+
+    public void SpawnEnemy(GameObject Enemy)
+    {
+        Instantiate(Enemy, transform.position, Quaternion.identity);
+        StartCoroutine(Spawning(Random.Range(2f, 3f)));
+    }
+}
