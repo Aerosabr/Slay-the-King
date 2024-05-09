ad48749d1b7f2adfebad5f9a4c6da7ecd049bebb
Jordan <jhn100550@gmail.com>
2024-05-08 19:02:09 -0700

    Progress towards enemies

--- Assets/Scripts/UI/CharacterCustomization.cs
+++ Assets/Scripts/UI/CharacterCustomization.cs
@@ -6,27 +6,13 @@ using UnityEngine.UI;
 
 public class CharacterCustomization : MonoBehaviour
 {
-	//Sprite Movement
-	public Vector2 _movementInput;
-	public Vector2 _smoothedMovementInput;
-	public Vector2 _movementInputSmoothVelocity;
-	public float sprintMultiplier = 1f;
-
-	//Sprite Animations
-	public WeaponAbilitySO[] weaponSO;
-	public int counter = 0;
-	public WeaponAbilitySO selectedWeaponSO;
+	public List<Animator> hairList = new List<Animator>();
+	public string hairColor = "3A3A3A";
+	public string skinColor = "FFE1AA";
+	public List<PlayerAssetLibrary> classList = new List<PlayerAssetLibrary>();
 
 	public List<Animator> Sprites = new List<Animator>();
 	public float walkSpeed = 1;
-	public Vector2 keyboardDirection;
-	public Vector2 currentDirection;
-
-	public bool isMoving;
-	public bool isSprinting;
-	public bool isAttacking;
-	public LayerMask interactableLayer;
-	public bool Movable = true;
 
 	private void Awake()
 	{
@@ -37,10 +23,6 @@ public class CharacterCustomization : MonoBehaviour
 		Sprites.Add(gameObject.GetComponent<Animator>());
 		for (int i = 0; i < 7; i++)
 			Sprites.Add(gameObject.transform.GetChild(i).GetComponent<Animator>());
-		for (int i = 0; i < 8; i++)
-		{
-			Sprites[i].runtimeAnimatorController = selectedWeaponSO.animatorBody[i].runtimeAnimatorController;
-		}
 	}
 
 	void FixedUpdate()
