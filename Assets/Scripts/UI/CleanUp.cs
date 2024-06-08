3d6462d729cfaa71b8c9fb492ea97254a6b4c60f
Tim Pham <43245834+phamt007@users.noreply.github.com>
2024-06-08 16:25:21 -0700

    CleanUp

---
+++ Assets/Scripts/UI/CleanUp.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CleanUp : MonoBehaviour
+{
+	public Transform PlayerHUD;
+	public Transform PlayerManager;
+	private void Awake()
+	{
+		GameObject playerHUDObject = GameObject.Find("PlayerHUD");
+		if (playerHUDObject != null)
+		{
+			PlayerHUD = playerHUDObject.transform;
+			Destroy(PlayerHUD.gameObject);
+		}
+
+		// Find PlayerManager and its children if it exists
+		GameObject playerManagerObject = GameObject.Find("PlayerManager");
+		if (playerManagerObject != null)
+		{
+			PlayerManager = playerManagerObject.transform;
+			if (PlayerManager.GetChild(0).childCount > 0)
+				Destroy(PlayerManager.GetChild(0).GetChild(0).gameObject);
+			if (PlayerManager.GetChild(1).childCount > 0)
+				Destroy(PlayerManager.GetChild(1).GetChild(0).gameObject);
+			if (PlayerManager.GetChild(2).childCount > 0)
+				Destroy(PlayerManager.GetChild(2).GetChild(0).gameObject);
+			if (PlayerManager.GetChild(3).childCount > 0)
+				Destroy(PlayerManager.GetChild(3).GetChild(0).gameObject);
+		}
+	}
+}
